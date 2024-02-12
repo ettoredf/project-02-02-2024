f38c3303ea146fdad9604060a92618fb8be97e2e
ettoredf <147718125+ettoredf@users.noreply.github.com>
2024-02-12 16:41:59 +0100

    Add files via upload

---
+++ Project-02-02-2024/Project-02-02-2024/Contribuente.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_02_02_2024
+{
+
+    // dichiarazione classe
+    public class Contribuente
+    {
+        public string Nome {  get; set; }
+        public string Cognome { get; set; }
+        public DateTime DataNascita { get; set; }
+        public string CodiceFiscale { get; set; }
+        public char Sesso { get; set; }
+        public string ComuneResidenza { get; set; }
+        public double RedditoAnnuale { get; set; }
+
+        // costruttore base
+        public Contribuente() { }
+
+        // costruttore personalizzato
+        public Contribuente(string nome, string cognome, DateTime dataNascita, string codiceFiscale, char sesso, string comuneResidenza, double redditoAnnuale)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            DataNascita = dataNascita;
+            CodiceFiscale = codiceFiscale;
+            Sesso = sesso;
+            ComuneResidenza = comuneResidenza;
+            RedditoAnnuale = redditoAnnuale;
+        }
+
+        // metodo per calcolo aliquote
+        public double CalcoloImposta()
+        {
+            double imposta = 0;
+
+            if (RedditoAnnuale <= 15000)
+            {
+                imposta = RedditoAnnuale * 0.23;
+            } 
+            else if (RedditoAnnuale > 15000 && RedditoAnnuale <= 28000)
+            {
+                imposta = 3450 + ((RedditoAnnuale - 15000) * 0.27);   
+            }
+            else if (RedditoAnnuale > 28000 && RedditoAnnuale <= 55000)
+            {
+                imposta = 6960 + ((RedditoAnnuale - 28000) * 0.38);
+            }
+            else if (RedditoAnnuale > 55000 && RedditoAnnuale <= 75000)
+            {
+                imposta = 17220 + ((RedditoAnnuale - 55000) * 0.41);
+            }
+            else
+            {
+                imposta = 25420 + ((RedditoAnnuale - 75000) * 0.43);
+            }
+
+            return imposta;
+        }
+    }
+}
---
+++ Project-02-02-2024/Project-02-02-2024/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_02_02_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Contribuente contribuente = new Contribuente();
+
+            // inserimento dati contribuente
+            Console.Write("Inserisci il nome: ");
+            contribuente.Nome = Console.ReadLine();
+
+            Console.Write("Inserisci il cognome: ");
+            contribuente.Cognome = Console.ReadLine();
+
+            Console.Write("Inserisci la data di nascita (formato YYYY-MM-DD): ");
+            contribuente.DataNascita = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Inserisci il codice fiscale: ");
+            contribuente.CodiceFiscale = Console.ReadLine();
+
+            Console.Write("Inserisci il sesso (M/F): ");
+            contribuente.Sesso = char.Parse(Console.ReadLine());
+
+            Console.Write("Inserisci il comune di residenza: ");
+            contribuente.ComuneResidenza = Console.ReadLine();
+
+            Console.Write("Inserisci il reddito annuale: ");
+            contribuente.RedditoAnnuale = double.Parse(Console.ReadLine());
+
+            // calcolo imposta
+            double imposta = contribuente.CalcoloImposta();
+
+            // riepilogo
+            Console.WriteLine("==================================================");
+
+            Console.WriteLine("CALCOLO DELL'IMPOSTA DA VERSARE:");
+
+            Console.WriteLine("\nRiepilogo Contribuente:");
+            Console.WriteLine($"Nome: {contribuente.Nome}");
+            Console.WriteLine($"Cognome: {contribuente.Cognome}");
+            Console.WriteLine($"Data di nascita: {contribuente.DataNascita.ToShortDateString()} ({contribuente.Sesso})");
+            Console.WriteLine($"Codice Fiscale: {contribuente.CodiceFiscale}");
+            Console.WriteLine($"Comune di residenza: {contribuente.ComuneResidenza}");
+            Console.WriteLine($"Reddito annuale:  {contribuente.RedditoAnnuale:C}");
+            Console.WriteLine($"Imposta da pagare:  {imposta:C}");
+            Console.ReadLine();
+        }
+    }
+}
